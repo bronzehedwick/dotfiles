598c28665e37b2f6f6b92a52fa2fd599a917b103
Chris DeLuca <bronzehedwick@gmail.com>
2012-08-22 14:37:54 -0400

    Added automation script to link dotfiles to home directory

---
+++ link_dotfiles.sh
@@ -0,0 +1,13 @@
+#! bin/bash
+FILES=.*
+TARGET=~
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+for f in $FILES
+do
+  if [[ -f $f ]]
+  then
+    echo "Linking $f to $TARGET"
+    ln -s $DIR/$f $TARGET/$f
+  fi
+done
+echo "Done"
