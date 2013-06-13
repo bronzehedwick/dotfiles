ef285ca92a70f2f788a2a90874d1295bba1737e2
Chris DeLuca <bronzehedwick@gmail.com>
2013-06-13 10:07:10 -0400

    Add non-working colortest alias.

--- bronzehedwick.zsh
+++ bronzehedwick.zsh
@@ -95,3 +95,4 @@ identica() {
 }
 alias dent="identica"
 
+alias colortest="( x=`tput op` y=`printf %$((${COLUMNS}-6))s`;for i in {0..256};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done; )"
