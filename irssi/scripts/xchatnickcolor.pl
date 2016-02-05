5b6fa1bbde8b259b6298aa2fae3ecd3e8f3ce57d
delucac <delucac@mskcc.org>
2016-02-05 17:11:13 -0500

    IRSSI updates.

--- irssi/scripts/xchatnickcolor.pl
+++ irssi/scripts/xchatnickcolor.pl
@@ -1,36 +1,43 @@
 use strict;
 use Irssi 20020101.0250 ();
 use vars qw($VERSION %IRSSI); 
-$VERSION = "2";
+$VERSION = "1";
 %IRSSI = (
-    authors     => "Timo Sirainen, Ian Peters, David Leadbeater",
-    contact	=> "tss\@iki.fi", 
+    authors     => "Timo Sirainen, Ian Peters",
+    contact => "tss\@iki.fi", 
     name        => "Nick Color",
     description => "assign a different color for each nick",
-    license	=> "Public Domain",
-    url		=> "http://irssi.org/",
-    changed	=> "Sun 15 Jun 19:10:44 BST 2014",
+    license => "Public Domain",
+    url     => "http://irssi.org/",
+    changed => "2002-03-04T22:47+0100"
 );
 
-# Settings:
-#   nickcolor_colors: List of color codes to use.
-#   e.g. /set nickcolor_colors 2 3 4 5 6 7 9 10 11 12 13
-#   (avoid 8, as used for hilights in the default theme).
+# hm.. i should make it possible to use the existing one..
+Irssi::theme_register([
+  'pubmsg_hilight', '{pubmsghinick $0 $3 $1}$2'
+]);
 
 my %saved_colors;
 my %session_colors = {};
+my @colors = qw/ 2 3 4 5 6 7 9 10 11 12 13 14 15/;
 
 sub load_colors {
-  open my $color_fh, "<", "$ENV{HOME}/.irssi/saved_colors";
-  while (<$color_fh>) {
-    chomp;
-    my($nick, $color) = split ":";
-    $saved_colors{$nick} = $color;
+  open COLORS, "$ENV{HOME}/.irssi/saved_colors";
+
+  while (<COLORS>) {
+    # I don't know why this is necessary only inside of irssi
+    my @lines = split "\n";
+    foreach my $line (@lines) {
+      my($nick, $color) = split ":", $line;
+      $saved_colors{$nick} = $color;
+    }
   }
+
+  close COLORS;
 }
 
 sub save_colors {
-  open COLORS, ">", "$ENV{HOME}/.irssi/saved_colors";
+  open COLORS, ">$ENV{HOME}/.irssi/saved_colors";
 
   foreach my $nick (keys %saved_colors) {
     print COLORS "$nick:$saved_colors{$nick}\n";
@@ -40,7 +47,8 @@ sub save_colors {
 }
 
 # If someone we've colored (either through the saved colors, or the hash
-# function) changes their nick, we'd like to keep the same color associated
+# function) changes their nick, we'd like to keep the same color
+# associated
 # with them (but only in the session_colors, ie a temporary mapping).
 
 sub sig_nick {
@@ -69,14 +77,19 @@ sub simple_hash {
     $counter += ord $char;
   }
 
-  my @colors = split / /, Irssi::settings_get_str('nickcolor_colors');
-  $counter = $colors[$counter % @colors];
+  $counter = $colors[$counter % 11];
 
   return $counter;
 }
 
+# FIXME: breaks /HILIGHT etc.
 sub sig_public {
   my ($server, $msg, $nick, $address, $target) = @_;
+  my $chanrec = $server->channel_find($target);
+  return if not $chanrec;
+  my $nickrec = $chanrec->nick_find($nick);
+  return if not $nickrec;
+  my $nickmode = $nickrec->{op} ? "@" : $nickrec->{voice} ? "+" : "";
 
   # Has the user assigned this nick a color?
   my $color = $saved_colors{$nick};
@@ -92,8 +105,11 @@ sub sig_public {
     $session_colors{$nick} = $color;
   }
 
-  $color = sprintf "\003%02d", $color;
-  $server->command('/^format pubmsg {pubmsgnick $2 {pubnick ' . $color . '$0}}$1');
+  $color = "0".$color if ($color < 10);
+  $server->command('/^format pubmsg %b<%w$2'.chr(3).$color.'$[-11]0%b>
+%K|%n $1');
+ # $server->command('/^format action_public {pubaction
+ # '.chr(3).$color.'$0}$1');
 }
 
 sub cmd_color {
@@ -103,7 +119,7 @@ sub cmd_color {
   $op = lc $op;
 
   if (!$op) {
-    Irssi::print ("No operation given (save/set/clear/list/preview)");
+    Irssi::print ("No operation given");
   } elsif ($op eq "save") {
     save_colors;
   } elsif ($op eq "set") {
@@ -126,7 +142,7 @@ sub cmd_color {
     Irssi::print ("\nSaved Colors:");
     foreach my $nick (keys %saved_colors) {
       Irssi::print (chr (3) . "$saved_colors{$nick}$nick" .
-		    chr (3) . "1 ($saved_colors{$nick})");
+          chr (3) . "1 ($saved_colors{$nick})");
     }
   } elsif ($op eq "preview") {
     Irssi::print ("\nAvailable colors:");
@@ -138,7 +154,6 @@ sub cmd_color {
 
 load_colors;
 
-Irssi::settings_add_str('misc', 'nickcolor_colors', '2 3 4 5 6 7 9 10 11 12 13');
 Irssi::command_bind('color', 'cmd_color');
 
 Irssi::signal_add('message public', 'sig_public');
