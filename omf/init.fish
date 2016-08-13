6b51db4ea7d86479b326d547d9b236d0b7c7f70b
Chris DeLuca <bronzehedwick@gmail.com>
2016-08-13 17:23:22 -0400

    -S
    Switch to using MacGPG.

--- omf/init.fish
+++ omf/init.fish
@@ -50,18 +50,7 @@ alias grt 'cd (git rev-parse --show-toplevel;or echo ".")'
 #######
 # GPG #
 #######
-if not begin
-  # Is the agent running already? Does the agent-info file exist, and if so,
-  # is there a process with the pid given in the file?
-  [ -f ~/.gpg-agent-info ]
-  and kill -0 (cut -d : -f 2 ~/.gpg-agent-info) ^/dev/null
-end
-# no, it is not running. Start it!
-gpg-agent --daemon --no-grab --write-env-file ~/.gpg-agent-info >/dev/null ^&1
-end
-# get the agent info from the info file, and export it so GPG can see it.
-set -gx GPG_AGENT_INFO (cut -c 16- ~/.gpg-agent-info)
-set -gx GPG_TTY (tty)
+set -gx PATH /usr/local/MacGPG2/bin/ $PATH
 
 #######################
 # Directory shortcuts #
