80902fb6c2b65f017d6597e7f48efd55f12373b6
Chris DeLuca <bronzehedwick@gmail.com>
2017-06-26 23:17:02 -0400

    Fix merge conflicts

--- emacs/init.el
+++ emacs/init.el
@@ -5,6 +5,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Packages
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (custom-set-variables
@@ -18,28 +19,36 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  )
+ )
 
-;; Require Org mode
+;; Change color scheme
+(require 'color-theme-sanityinc-tomorrow)
+(load-theme 'sanityinc-tomorrow-eighties)
+
+;;
+;; Org Mode
+;;
+
+;; Bindings
 (require 'org)
-
-;; Org mode bindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (define-key global-map "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-;; Org mode templates
+;; Todo Keywords
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "STARTED(s)" "WAITING(f)" "DELEGATED(g)" "APPT(a)" "|" "DONE(d)" "DEFFERED(f)" "CANCELED(c)")))
+
+;; Tags
+(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@art" . ?a))
+
+;; Capture Templates
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "" "Tasks")
-	 "* TODO %?\n %i\n %a")
-	("n" "Note" entry (file+headline "" "Notes")
-	 "* %?\nentered on %U\n %i\n %^g")))
-
-;; Org mode encryption
-(setq epa-file-encrypt-to "bronzehedwick@gmail.com")
-
-;; Change color scheme
-(require 'color-theme-sanityinc-tomorrow)
-(color-theme-sanityinc-tomorrow-eighties)
+      (quote (("t" "Todo" entry (file+headline (concat org-directory "/todos.org") "Tasks")
+	       "* TODO %? %^g\n%^t\n%i")
+	      ("n" "Note" entry (file+headline (concat org-directory "/notes.org") "Notes")
+	       "* %?\n%U\n%i\n")
+	      ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+	       "* %?\nEntered on %U\n%i\n"))))
