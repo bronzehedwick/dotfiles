946a2cbd121b9d2446ced39f0b29f2d4ae22b441
Chris DeLuca <delucac@mskcc.org>
2018-03-06 16:43:51 -0500

    Move vim-plug installation into neovim config
    As per the vim-plug documentation, move the vim-plug installation to
    inside neovim config, instead of my custom dotfiles install script.

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 # Installs dotfiles to home directory
 
-# Install VimPlug
-if [ ! -d "$HOME/.config/nvim/autoload" ]; then
-  curl -fLo "$HOME/.config/nvim/autoload/plug.vim" --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
 # Find all non-hidden, top-level directories in this folder.
 find "$(dirname "$0")" -type d -depth 1 -name "[^.]*" | while read -r program;
 do
