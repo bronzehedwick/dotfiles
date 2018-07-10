016644fdbc011c897e5826297f81681dea8891df
Chris DeLuca <iam@chrisdeluca.me>
2018-07-09 22:51:43 -0400

    Add neomutt config

---
+++ offlineimap/.offlineimap.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+
+from subprocess import check_output
+
+def macosgetpass():
+    return check_output("security find-generic-password -w -s \"fastmail_offlineimap\"", shell=True).strip("\n")
