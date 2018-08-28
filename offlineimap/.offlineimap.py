a2ff65980e2fd7d47352ee489a915ccef0cb383d
Chris DeLuca <iam@chrisdeluca.me>
2018-08-28 10:01:40 -0400

    Remove MSK mutt config
    It's too much of a pain in the ass to do MSK email as it is, much less using DavMail and neomutt at the same time. Remove.

--- offlineimap/.offlineimap.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python2
-
-from subprocess import check_output
-
-def macosgetpass():
-    return check_output("security find-generic-password -w -s fastmail_offlineimap", shell=True).strip("\n")
