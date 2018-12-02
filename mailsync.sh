b558787330119574d0882332536b1566b9f9c659
Chris DeLuca <iam@chrisdeluca.me>
2018-12-02 12:37:15 -0500

    Make mailsync tagging more concise
    Use notmuch's batch syntax to shorten the code for email tagging on
    sync.
    
    Remove the lists tagging, and remove from that virtual mailbox from
    neomutt. I wasn't really using it and I am using a different method to
    handling newsgroups.
    
    Switch to a more concise way of setting the Trash.

--- mailsync.sh
+++ mailsync.sh
@@ -6,17 +6,11 @@
 # do initial tagging
 /usr/local/bin/notmuch new
 
-# retag all "new" messages in INBOX as "inbox"
-/usr/local/bin/notmuch tag +inbox -new folder:INBOX
-
-# retag all messages in Archive as "archive"
-/usr/local/bin/notmuch tag +archive -inbox folder:Archive
-
-# retag all messages in Junk as "spam"
-/usr/local/bin/notmuch tag +spam -inbox folder:Junk
-
-# retag all messages in Trash as "deleted"
-/usr/local/bin/notmuch tag +deleted -inbox -archive folder:Trash
-
-# tag newsletters, defined as anything with the phrase "unsubscribe"
-/usr/local/bin/notmuch tag +lists -inbox -new unsubscribe
+# retag mails according to Maildir format
+notmuch tag --batch <<EOF
+    +inbox -- folder:INBOX
+    -inbox -new -- not folder:INBOX
+    +spam -- folder:Junk
+    -spam -- not folder:Junk
+    +archive -- folder:Archive
+EOF
