ea7ec9397221dcfb59fc2d12b4183fddc38e375c
Chris DeLuca <iam@chrisdeluca.me>
2019-01-08 10:48:45 -0500

    Clean up linking script
    Linking script has been copied into the Makefile, so remove the separate
    script.
    
    Move the ~/Mail directory creation to mailsync.sh.

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# sync mail from IMAP
+# Add the Mail directory if it doesn't already exist.
+mkdir -p ~/Mail
+
+# Sync mail from IMAP.
 /usr/local/bin/mbsync chris
 
-# do initial tagging
+# Do initial tagging.
 /usr/local/bin/notmuch new
 
-# retag mails according to Maildir format
+# Retag mails according to Maildir format.
 notmuch tag --batch <<EOF
     +inbox -- folder:INBOX
     -inbox -new -- not folder:INBOX
