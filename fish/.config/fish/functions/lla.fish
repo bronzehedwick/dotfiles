85acfcafda932665605b093afc6a55b76f8ee932
Chris DeLuca <iam@chrisdeluca.me>
2020-04-29 15:35:30 -0400

    Fix fish lla function

--- fish/.config/fish/functions/lla.fish
+++ fish/.config/fish/functions/lla.fish
@@ -1,6 +1,8 @@
 function lla --description "Replace lla - long all listing - with exa if it exists"
-  command exa --long --all $argv
-else
-  command ls -AFchl $argv
+  if test -x /usr/local/bin/exa
+    command exa --long --all $argv
+  else
+    command ls -AFchl $argv
+  end
 end
 
