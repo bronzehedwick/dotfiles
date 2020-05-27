56487331c1fdd21e224254d5710cac09cd081a34
Chris DeLuca <iam@chrisdeluca.me>
2020-05-27 10:36:42 -0400

    Add bash support for fish
    Enable this by using the fisher fish package manager. This allows the
    use of nvm in fish.
    
    Also, add a new method of reverse search. It's only okay, but works
    better than the hacked together solution I half made (in that it works).
    I wish you could see a few more results of your search, not just one,
    but it's still an improvement.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,14 +14,14 @@ if test -x /usr/local/bin/yarn
   set PATH ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
 end
 
-if test -e "$NVIM_LISTEN_ADDRESS+x"
+if test -e $NVIM_LISTEN_ADDRESS
   if test -x /usr/local/bin/page
     set PAGER /usr/local/bin/page
   end
 end
 
-# Keybindings
-
-if test -x /usr/local/bin/fzy
-  bind \cr 'set hist_cmd (history | fzy) && eval $hist_cmd'
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
 end
