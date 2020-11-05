721dbc3786ffefaba26f64f1a7e6189ad475fd45
Chris DeLuca <code@chrisdeluca.me>
2020-11-05 14:46:13 -0500

    Add EDITOR variables outside neovim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,7 +14,12 @@ if test -x /usr/local/bin/yarn
   set PATH ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
 end
 
-if test -e $NVIM_LISTEN_ADDRESS
+set EDITOR nvim
+set VISUAL nvim
+
+if test -n "$NVIM_LISTEN_ADDRESS"
+  set EDITOR nvr
+  set VISUAL nvr
   if test -x /usr/local/bin/page
     set PAGER /usr/local/bin/page
   end
