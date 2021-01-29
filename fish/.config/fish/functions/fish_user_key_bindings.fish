8c7d5f88c57dd432f0f43a8a3203fc472fe48345
Chris DeLuca <code@chrisdeluca.me>
2021-01-29 10:15:21 -0500

    Add fish bindings
    Put emphasis on backwards deleting, and add back standard emacs downcase
    binding instead of being an alias to `ls`.
    
    Also replace non-working plugin call for history to use a built-in fish
    function that's similar.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
-  bind \cr re_search
+  bind \cr history-prefix-search-backward
+  bind \cf forward-bigword
+  bind \cb backward-bigword
+  bind \cd backward-kill-bigword
+  bind \ed backward-kill-word
+  bind \el downcase-word
 end
 
