39e76450015603c6a981005ca53572701ed87330
Chris DeLuca <code@chrisdeluca.me>
2021-02-01 09:22:33 -0500

    Remove some fish bindings
    I got too enthusiastic about rebindings, which trample on features that are really useful. Remove.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,7 @@
 function fish_user_key_bindings
   bind \cr history-prefix-search-backward
-  bind \cf forward-bigword
   bind \cb backward-bigword
   bind \cd backward-kill-bigword
-  bind \ed backward-kill-word
   bind \el downcase-word
 end
 
