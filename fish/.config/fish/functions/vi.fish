c8aacd4b4850e60937c910d30fc9a041e7583932
Chris DeLuca <code@chrisdeluca.me>
2021-03-02 11:49:30 -0500

    Delete vi/vim fish aliases
    These conflict with some defaults and I never use them. Might be a red
    herring on the first point, but not using them is a good enough reason
    to remove.

--- fish/.config/fish/functions/vi.fish
+++
@@ -1,8 +0,0 @@
-function vi --description "Alias vi to nvim"
-  if test -n "$NVIM_LISTEN_ADDRESS"
-    command nvr $argv
-  else
-    command nvim $argv
-  end
-end
-
--- fish/.config/fish/functions/vim.fish
+++
@@ -1,8 +0,0 @@
-function vim --description "Alias vim to nvim"
-  if test -n "$NVIM_LISTEN_ADDRESS"
-    command nvr $argv
-  else
-    command nvim $argv
-  end
-end
-
