a6a5cf8cde9e13dd408ddeec58fb34681ae5ce9d
Chris DeLuca <code@chrisdeluca.me>
2021-03-12 09:56:44 -0500

    Set MANPAGER as neovim inside a neovim instance

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,16 +14,21 @@ if test -x /usr/local/bin/yarn
   set PATH ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
 end
 
+# Neovim settings
+
 if test -z "$NVIM_LISTEN_ADDRESS"
   set EDITOR nvim --noplugin
   set VISUAL nvim --noplugin
 end
 
 if test -n "$NVIM_LISTEN_ADDRESS"
+  set MANPAGER "/usr/local/bin/nvr -c 'set ft=man' -o -"
   if test -x /usr/local/bin/page
     set PAGER /usr/local/bin/page
   end
 end
+
+# Fisher plugins
 
 if not functions -q fisher
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
