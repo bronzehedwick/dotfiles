59f2a17beedd1e04b119d251d1bdd59ba84a0dc4
Chris DeLuca <code@chrisdeluca.me>
2021-03-22 14:36:07 -0400

    Add jumpy.vim
    It's the [[ and ]] I always wanted! Or, the functionality I expected from the vim docs! Close enough! Also, sort the plugin list and indent properly.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -3,42 +3,43 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
 
-  -- Packer can manage itself as an optional plugin
-  use {'wbthomason/packer.nvim', opt = true}
+    -- Packer can manage itself as an optional plugin
+    use {'wbthomason/packer.nvim', opt = true}
 
-  -- Simple plugins can be specified as strings
-  use 'MaxMEllon/vim-jsx-pretty'
-  use 'arzg/vim-colors-xcode'
-  use 'bronzehedwick/msmtp-syntax.vim'
-  use 'bronzehedwick/vim-primary-terminal'
-  use 'cespare/vim-toml'
-  use 'chr4/nginx.vim'
-  use 'dag/vim-fish'
-  use 'freitass/todo.txt-vim'
-  use 'justinmk/vim-dirvish'
-  use 'justinmk/vim-ipmotion'
-  use 'justinmk/vim-sneak'
-  use 'mbbill/undotree'
-  use 'neovim/nvim-lspconfig'
-  use 'othree/html5.vim'
-  use 'pangloss/vim-javascript'
-  use 'rstacruz/vim-closer'
-  use 'srstevenson/vim-picker'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-eunuch'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-rhubarb'
-  use 'tpope/vim-rsi'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-unimpaired'
-  use 'vim-scripts/fountain.vim'
-  use 'whiteinge/diffconflicts'
-  use { 'mattn/emmet-vim', opt = true }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'plasticboy/vim-markdown', opt = true }
-  use { 'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'} }
-  use { 'andymass/vim-matchup', event = 'VimEnter' }
+    -- Simple plugins can be specified as strings
+    use 'MaxMEllon/vim-jsx-pretty'
+    use 'arp242/jumpy.vim'
+    use 'arzg/vim-colors-xcode'
+    use 'bronzehedwick/msmtp-syntax.vim'
+    use 'bronzehedwick/vim-primary-terminal'
+    use 'cespare/vim-toml'
+    use 'chr4/nginx.vim'
+    use 'dag/vim-fish'
+    use 'freitass/todo.txt-vim'
+    use 'justinmk/vim-dirvish'
+    use 'justinmk/vim-ipmotion'
+    use 'justinmk/vim-sneak'
+    use 'mbbill/undotree'
+    use 'neovim/nvim-lspconfig'
+    use 'othree/html5.vim'
+    use 'pangloss/vim-javascript'
+    use 'rstacruz/vim-closer'
+    use 'srstevenson/vim-picker'
+    use 'tpope/vim-commentary'
+    use 'tpope/vim-endwise'
+    use 'tpope/vim-eunuch'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-repeat'
+    use 'tpope/vim-rhubarb'
+    use 'tpope/vim-rsi'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-unimpaired'
+    use 'vim-scripts/fountain.vim'
+    use 'whiteinge/diffconflicts'
+    use { 'andymass/vim-matchup', event = 'VimEnter' }
+    use { 'mattn/emmet-vim', opt = true }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 'plasticboy/vim-markdown', opt = true }
+    use { 'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'} }
 
 end)
