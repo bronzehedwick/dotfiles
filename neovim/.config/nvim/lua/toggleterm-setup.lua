e080fde00efb1ede7f29bf1570ae72eecc7f83d6
Chris DeLuca <code@chrisdeluca.me>
2021-04-14 11:59:40 -0400

    Switch to full screen toggle term

--- neovim/.config/nvim/lua/toggleterm-setup.lua
+++ neovim/.config/nvim/lua/toggleterm-setup.lua
@@ -1,8 +1,7 @@
 require"toggleterm".setup{
-  size = 20,
   open_mapping = [[<c-s>]],
   shade_terminals = false,
   start_in_insert = true,
   persist_size = true,
-  direction = 'horizontal',
+  direction = 'window',
 }
