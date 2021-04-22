41251b5aa4b23533310558970b51fbd71b50b060
Chris DeLuca <code@chrisdeluca.me>
2021-04-22 11:19:39 -0400

    Use float terminal
    I like how it centers in view. I do not like how I cannot use gf with ease the same way I would in a non-float terminal, but maybe I can work around that, or create two mappings, one for float and one for a split or window. They should both use the same terminal, tho.

--- neovim/.config/nvim/lua/toggleterm-setup.lua
+++ neovim/.config/nvim/lua/toggleterm-setup.lua
@@ -1,7 +1,18 @@
 require"toggleterm".setup{
   open_mapping = [[<c-s>]],
+  hide_numbers = true,
   shade_terminals = false,
   start_in_insert = true,
   persist_size = true,
-  direction = 'window',
+  direction = 'float',
+  float_opts = {
+    border = 'single',
+      width = 78,
+      height = 30,
+      winblend = 3,
+      highlights = {
+        border = "Normal",
+        background = "Normal",
+      }
+  }
 }
