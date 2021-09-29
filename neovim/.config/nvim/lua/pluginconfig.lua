d28111e47b035625b6d25d55fffd8e9a18361838
Chris DeLuca <code@chrisdeluca.me>
2021-09-29 16:39:26 -0400

    Add LSP config comment for omnicomplete

--- neovim/.config/nvim/lua/pluginconfig.lua
+++ neovim/.config/nvim/lua/pluginconfig.lua
@@ -11,6 +11,7 @@ local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
+  -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
