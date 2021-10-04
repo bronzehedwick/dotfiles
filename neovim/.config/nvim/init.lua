1e59f830305bdfe01d5dbe1d3cc4b314b32315a3
Chris DeLuca <code@chrisdeluca.me>
2021-10-04 14:52:54 -0400

    Abstract lua utilities
    Make a new file for lua helpers. Right now it's just the `map` function,
    but that will probably grow. Even with just map, this lets me include
    mappings in other runtime files outside init.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,22 +1,6 @@
 -- Helpers {{{
 
-local map = function(key)
-  -- get the extra options
-  local opts = {noremap = true}
-  for i, v in pairs(key) do
-    if type(i) == 'string' then opts[i] = v end
-  end
-
-  -- basic support for buffer-scoped keybindings
-  local buffer = opts.buffer
-  opts.buffer = nil
-
-  if buffer then
-    vim.api.nvim_buf_set_keymap(0, key[1], key[2], key[3], opts)
-  else
-    vim.api.nvim_set_keymap(key[1], key[2], key[3], opts)
-  end
-end
+local map = require'utilities'.map
 
 -- }}}
 
