7a77e76626efb9c57c981e47487fe8297cc15759
Chris DeLuca <code@chrisdeluca.me>
2021-11-04 21:38:18 -0400

    Add augroup lua utility function

--- neovim/.config/nvim/lua/utilities.lua
+++ neovim/.config/nvim/lua/utilities.lua
@@ -18,4 +18,17 @@ M.map = function(key)
   end
 end
 
+-- Taken from https://github.com/norcalli/nvim_utils
+M.create_augroup = function (definitions)
+  for group_name, definition in pairs(definitions) do
+    vim.api.nvim_command('augroup ' .. group_name)
+    vim.api.nvim_command('autocmd!')
+    for _, def in ipairs(definition) do
+      local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+      vim.api.nvim_command(command)
+    end
+    vim.api.nvim_command('augroup END')
+  end
+end
+
 return M
