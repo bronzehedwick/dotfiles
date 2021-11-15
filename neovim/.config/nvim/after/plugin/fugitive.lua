3f2402c7ae2c603265b95117fb2874c8efa15745
Chris DeLuca <code@chrisdeluca.me>
2021-11-15 12:36:18 -0500

    Convert fugitive to lua

---
+++ neovim/.config/nvim/after/plugin/fugitive.lua
@@ -0,0 +1,58 @@
+-- Exit if fugitive isn't loaded.
+if (vim.fn.exists(':Git') == 1) then
+  return
+end
+
+local map = require'utilities'.map
+
+-- Status line. {{{
+-- Git branch.
+vim.opt.statusline='%{FugitiveStatusline()}'
+-- Tail of file (just the name.ext).
+vim.opt.statusline:append('%< %t')
+-- File modified flag.
+vim.opt.statusline:append('%m')
+-- Buffer is `help` flag.
+vim.opt.statusline:append('%< %h')
+-- Buffer is `readonly` flag.
+vim.opt.statusline:append('%< %r')
+-- Buffer is `preview` flag.
+vim.opt.statusline:append('%< %w')
+-- New group.
+vim.opt.statusline:append('%=')
+-- Line and column number.
+vim.opt.statusline:append('%l')
+-- New group.
+vim.opt.statusline:append('%=')
+-- Percentage through the file.
+vim.opt.statusline:append('%p%%')
+-- }}}
+
+-- Fugitive commands. {{{
+vim.cmd [[
+command! -bang -bar -nargs=* Gpush execute 'Dispatch<bang> -dir=' .
+      \ fnameescape(FugitiveWorkTree()) 'git push' <q-args>
+command! -bang -bar -nargs=* Gfetch execute 'Dispatch<bang> -dir=' .
+      \ fnameescape(FugitiveWorkTree()) 'git fetch' <q-args>
+command! -bang -bar -nargs=* Gup execute 'Dispatch<bang> -dir=' .
+      \ fnameescape(FugitiveWorkTree()) 'git up' <q-args>
+command! -bang -bar -nargs=* Gpo execute 'Dispatch<bang> -dir=' .
+      \ fnameescape(FugitiveWorkTree()) 'git po' <q-args>
+command! -bang -bar -nargs=* Gph execute 'Dispatch<bang> -dir=' .
+      \ fnameescape(FugitiveWorkTree()) 'git ph' <q-args>
+]]
+-- }}}
+
+-- Fugitive mappings. {{{
+map {'n', '<M-g>s', ':Git<CR>', silent = true}
+map {'n', '<M-g>d', ':Gdiff<CR>', silent = true}
+map {'n', '<M-g>c', ':Git commit<CR>', silent = true}
+map {'n', '<M-g>b', ':Git blame<CR>', silent = true}
+map {'n', '<M-g>l', ':Gclog<CR>', silent = true}
+map {'n', '<M-g>p', ':Gpo<CR>', silent = true}
+map {'n', '<M-g>r', ':Gread<CR>', silent = true}
+map {'n', '<M-g>w', ':Gwrite<CR>', silent = true}
+map {'n', '<M-g>e', ':Gedit<CR>', silent = true}
+map {'n', '<M-g>u', ':Gup<CR>', silent = true}
+map {'n', '<M-g>f', ':Gfetch<CR>', silent = true}
+-- }}}
