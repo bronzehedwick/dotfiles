432741226e4eeeaed71120d7a11c76b384884849
Chris DeLuca <code@chrisdeluca.me>
2021-11-15 15:36:26 -0500

    Add PHP LSP configuration

--- neovim/.config/nvim/lua/pluginconfig.lua
+++ neovim/.config/nvim/lua/pluginconfig.lua
@@ -136,7 +136,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "cssls", "html", "tsserver", "jsonls" }
+local servers = { "cssls", "html", "tsserver", "jsonls", "phpactor" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
