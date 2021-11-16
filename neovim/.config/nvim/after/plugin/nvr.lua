2a98435650ad14d2ff68466e03162dfeaacbe560
Chris DeLuca <code@chrisdeluca.me>
2021-11-16 11:56:15 -0500

    Convert nvr config to lua

---
+++ neovim/.config/nvim/after/plugin/nvr.lua
@@ -0,0 +1,8 @@
+-- Exit if not neovim and nvr isn't present.
+if (not vim.fn.executable('nvr')) then
+  return
+end
+
+-- Defaults for how nvr is called from other programs.
+vim.env.EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+vim.env.VISUAL = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
