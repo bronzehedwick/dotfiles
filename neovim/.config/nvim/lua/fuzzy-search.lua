eb01eba510e44d80d0ae380a4d8beb532352201d
Chris DeLuca <code@chrisdeluca.me>
2021-11-17 12:00:47 -0500

    Make fuzzy search matches adhere to window height

--- neovim/.config/nvim/lua/fuzzy-search.lua
+++ neovim/.config/nvim/lua/fuzzy-search.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.FuzzySearch = function()
   local width = 55
-  local height = 15
+  local height = 11
 
   vim.api.nvim_open_win(
     vim.api.nvim_create_buf(false, true),
@@ -26,7 +26,7 @@ M.FuzzySearch = function()
   git_branch = string.gsub(git_branch, '%s+$', '')
   git_branch = string.gsub(git_branch, '[\n\r]+', '')
 
-  vim.fn.termopen('git ls-tree -r --name-only ' .. git_branch .. '|fzy|xargs nvr --nostart --remote -cc "lua vim.api.nvim_win_close(vim.fn.win_getid(), true)" -l')
+  vim.fn.termopen('git ls-tree -r --name-only ' .. git_branch .. '|fzy --lines ' .. (height - 1) .. '|xargs nvr --nostart --remote -cc "lua vim.api.nvim_win_close(vim.fn.win_getid(), true)" -l')
   vim.api.nvim_command('startinsert')
 end
 
