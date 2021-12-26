997fb38998de545d208b2d2a3c799d7416322a7d
Chris DeLuca <code@chrisdeluca.me>
2021-12-26 13:08:25 -0500

    Clean up variables for removed vim-markdown
    These variables are only used in `vim-markdown`, which I have removed.
    Good plugin, but trying to keep my config lean and mean.

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -16,13 +16,6 @@ vim.g.markdown_fenced_languages = {
   'css'
 }
 
--- Support YAML and TOML front matter syntax highlighting, via vim-markdown.
-vim.g.vim_markdown_frontmatter = 1
-vim.g.vim_markdown_toml_frontmatter = 1
-
--- If an H1 is used as a file heading, don't fold it, via vim-markdown.
-vim.g.vim_markdown_folding_style_pythonic = 1
-
 -- Use a dictionary to lookup words.
 if vim.fn.executable('dict') then
   vim.opt.keywordprg = 'dict'
