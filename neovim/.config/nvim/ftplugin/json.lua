7e63a76dcc90f1123f8c6b6412a75d3fb8a1d762
Chris DeLuca <code@chrisdeluca.me>
2022-02-16 13:13:26 -0500

    Make json sw default 4

--- neovim/.config/nvim/ftplugin/json.lua
+++ neovim/.config/nvim/ftplugin/json.lua
@@ -1,3 +1,6 @@
+-- Use 4 space indents.
+vim.opt.shiftwidth = 4
+
 -- Use jsonlint if available, otherwise use python's built-in json linter.
 if vim.fn.executable('jsonlint') then
   vim.cmd('compiler jsonlint')
