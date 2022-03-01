0dfcdf04b8545ea6c0cb796e67e770c0b68ca9ea
Chris DeLuca <code@chrisdeluca.me>
2022-03-01 10:57:46 -0500

    Disable orgmode formatexpr

--- neovim/.config/nvim/ftplugin/org.lua
+++ neovim/.config/nvim/ftplugin/org.lua
@@ -9,5 +9,10 @@ if vim.fn.executable('dict') then
   -- nnoremap <buffer> <silent> <S-k> :execute "split " . shellescape(&keywordprg) . "<bar> 0read !" . shellescape(&keywordprg) . " " . expand("<cword>")<bar> :Man!<CR>
 end
 
+-- Do not use the orgmode plugin's formatexpr, since it seems to only
+-- be used for date format operations I don't care about, and it tramples
+-- `gq`, which I do care bout.
+vim.opt.formatexpr = ''
+
 -- Insert time in this format: [2021-09-01 Wed]
 map {'i', '<C-g><C-t>', '<C-r>=strftime("[%Y-%m-%d %a]")<CR>'}
