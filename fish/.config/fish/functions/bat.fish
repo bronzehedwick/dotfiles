3f952c48e3942a7713c49734eecc3d11b49ac8bb
Chris DeLuca <code@chrisdeluca.me>
2022-04-14 16:15:24 -0400

    Remove bat fish alias
    I no longer install this program, so I can remove my customizations.
    Also, this is where we are with tech, where the phrase "bat fish alias"
    means something.

--- fish/.config/fish/functions/bat.fish
+++
@@ -1,7 +0,0 @@
-function bat --description "Dynamically set bat theme based on os theme"
-  if test (/usr/local/bin/dark-notify --exit) = "light"
-    command bat --theme=OneHalfLight $argv
-  else
-    command bat --theme=OneHalfDark $argv
-  end
-end
