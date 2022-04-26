1bb03f74dc2b7bdd8ac71383f6511e93bf0b94be
Chris DeLuca <code@chrisdeluca.me>
2022-04-26 14:29:30 -0400

    Remove custom map function
    This has been superannuated by `vim.keymap.set()`.

--- neovim/.config/nvim/lua/utilities.lua
+++ neovim/.config/nvim/lua/utilities.lua
@@ -1,23 +1,5 @@
 local M = {}
 
-M.map = function(key)
-  -- get the extra options
-  local opts = {noremap = true}
-  for i, v in pairs(key) do
-    if type(i) == 'string' then opts[i] = v end
-  end
-
-  -- basic support for buffer-scoped keybindings
-  local buffer = opts.buffer
-  opts.buffer = nil
-
-  if buffer then
-    vim.api.nvim_buf_set_keymap(0, key[1], key[2], key[3], opts)
-  else
-    vim.api.nvim_set_keymap(key[1], key[2], key[3], opts)
-  end
-end
-
 -- Taken from https://github.com/norcalli/nvim_utils
 M.create_augroups = function (definitions)
   for group_name, definition in pairs(definitions) do
