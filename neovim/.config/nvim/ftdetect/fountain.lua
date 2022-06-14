0807adbcbdd0e481e287ed3468d91f185c191d40
Chris DeLuca <code@chrisdeluca.me>
2022-06-14 11:49:21 -0400

    Add more drupal filetype detects
    Set `.theme` and `.install` as PHP.

--- neovim/.config/nvim/ftdetect/fountain.lua
+++ neovim/.config/nvim/ftdetect/fountain.lua
@@ -2,5 +2,4 @@ vim.cmd [[
   augroup fountain
     autocmd BufNewFile,BufRead *.fountain set filetype=fountain
   augroup END
-
 ]]
--- neovim/.config/nvim/ftdetect/module.lua
+++ neovim/.config/nvim/ftdetect/module.lua
@@ -2,5 +2,7 @@
 vim.cmd [[
   augroup drupal
     autocmd BufRead,BufNewFile *.module set filetype=php
+    autocmd BufRead,BufNewFile *.theme set filetype=php
+    autocmd BufRead,BufNewFile *.install set filetype=php
   augroup END
 ]]
