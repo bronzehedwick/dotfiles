454c2985e21a9c5216033dba0fc99d18ff254cd5
Chris DeLuca <code@chrisdeluca.me>
2022-08-07 11:12:59 -0400

    Refactor gdelmerged
    This syntax works better?

--- fish/.config/fish/functions/gdelmerged.fish
+++ fish/.config/fish/functions/gdelmerged.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 function gdelmerged --description "Deletes all branches merged into current branch (excluding main, master, and dev)."
-  git branch --merged | grep -E -v "(^\\*|master|main|dev)" | xargs git branch -d
+  git branch --merged | grep -v -e master -e main -e develop | xargs git branch -d
 end
