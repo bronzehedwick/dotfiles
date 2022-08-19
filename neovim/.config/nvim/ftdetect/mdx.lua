a56ac2b1bf67282bb603a4111e255df539074886
Chris DeLuca <code@chrisdeluca.me>
2022-08-19 07:36:33 -0400

    Treat mdx files as markdown

---
+++ neovim/.config/nvim/ftdetect/mdx.lua
@@ -0,0 +1,8 @@
+local create_augroups = require'utilities'.create_augroups
+local autocmds = {}
+
+autocmds.mdx = {
+  {'BufNewFile,BufRead', '*.mdx', 'set filetype=markdown'},
+}
+
+create_augroups(autocmds)
