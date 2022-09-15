e8c3ffa3787a0693e0c1258e4be3d6231be8b058
Chris DeLuca <code@chrisdeluca.me>
2022-09-15 16:08:30 -0400

    Turn off treesitter folding

--- neovim/.config/nvim/lua/configurations.lua
+++ neovim/.config/nvim/lua/configurations.lua
@@ -363,8 +363,8 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.opt.foldmethod = 'expr'
+-- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
 -- }}}
 
