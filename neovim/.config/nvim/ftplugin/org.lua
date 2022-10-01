223f3e9abaacf9bd439519024ea8aaa4e90eaf65
Chris DeLuca <code@chrisdeluca.me>
2022-10-01 13:56:03 -0400

    Org filetype tweaks
    Use the org mode provided `formatexpr`, since it has grown useful
    features now specific to org files.
    
    Set conceal on for org files again. Maybe I'll end up liking this now.

--- neovim/.config/nvim/ftplugin/org.lua
+++ neovim/.config/nvim/ftplugin/org.lua
@@ -1,16 +1,15 @@
 -- Enable spell checking.
 vim.cmd('setlocal spell')
 
+-- Set conceal
+vim.cmd('setlocal conceallevel=2')
+vim.cmd('setlocal concealcursor=nc')
+
 -- Use a dictionary to lookup words.
 if vim.fn.executable('dict') then
   vim.opt.keywordprg = 'dict'
   -- nnoremap <buffer> <silent> <S-k> :execute "split " . shellescape(&keywordprg) . "<bar> 0read !" . shellescape(&keywordprg) . " " . expand("<cword>")<bar> :Man!<CR>
 end
-
--- Do not use the orgmode plugin's formatexpr, since it seems to only
--- be used for date format operations I don't care about, and it tramples
--- `gq`, which I do care bout.
-vim.opt.formatexpr = ''
 
 -- Insert time in this format: [2021-09-01 Wed]
 vim.keymap.set('i', '<C-g><C-t>', '<C-r>=strftime("[%Y-%m-%d %a]")<CR>')
