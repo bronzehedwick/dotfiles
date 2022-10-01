48b6482aa70b0b0f3102f97c98cb3ad01bbfd458
Chris DeLuca <code@chrisdeluca.me>
2022-10-01 13:58:09 -0400

    Remove filetype lua variable overrides
    Now that 0.8 is out, I no longer need these.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
--- Use filetype.lua for performance.
--- Remove these at the next release.
--- See https://github.com/neovim/neovim/issues/14090#issuecomment-1177933661
-vim.g.did_load_filetypes = 0
-vim.g.do_filetype_lua = 1
-
 require'configurations'
 require'mappings'
 require'plugins'
