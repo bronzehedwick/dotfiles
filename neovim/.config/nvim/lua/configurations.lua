78105dd76b85b8abdab2511adf5107ed9ea494e0
Chris DeLuca <code@chrisdeluca.me>
2022-10-07 10:28:49 -0400

    Format LSP server configuration
    Easier to read and modify. It feels like my LSP names/servers are out of
    date, though. More investigation to come.

--- neovim/.config/nvim/lua/configurations.lua
+++ neovim/.config/nvim/lua/configurations.lua
@@ -295,7 +295,14 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "cssls", "html", "tsserver", "jsonls", "phpactor", "pyright" }
+local servers = {
+  "cssls",
+  "html",
+  "tsserver",
+  "jsonls",
+  "phpactor",
+  "pyright"
+}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
