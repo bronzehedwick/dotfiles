343c0fd6eaae79945caabcc7d7a0d86018595ee0
Chris DeLuca <code@chrisdeluca.me>
2022-10-10 20:01:10 -0400

    Fix twig commenting for treesitter

--- neovim/.config/nvim/ftplugin/htmldjango.lua
+++ neovim/.config/nvim/ftplugin/htmldjango.lua
@@ -1,18 +1,13 @@
 -- Load emmet HTML quickwrite plugin.
 vim.cmd('packadd emmet-vim')
 
--- Add twig pattern files to path to be able to configure below. Enables `gf`
--- on include to jump to that file. MSK specific.
-vim.opt.path:append('/Users/delucac/Sites/msk-design-system/src/patterns/')
-
 -- Set pattern for vim to recognize twig includes.
 vim.opt.include = "^\\s*{%\\s*include|^\\s*{%\\s*embed|^\\s*{%\\s*extends"
 
--- Point to design system. MSK specific.
-vim.opt.includeexpr = vim.fn.substitute('v:fname', 'mskds', '~/Sites/msk-design-system/src/patterns', '')
-
 -- Use twig commenting instead of HTML.
 vim.opt.commentstring="{# %s #}"
+local ft = require('Comment.ft')
+ft.set('htmldjango', '{#%s#}')
 
 -- Use twiglint linter.
 vim.cmd('compiler twiglint')
