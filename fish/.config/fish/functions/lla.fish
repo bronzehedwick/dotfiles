1040cc7da4a80e0d02758a1d5ae46436e6c86f68
Chris DeLuca <code@chrisdeluca.me>
2022-11-07 21:16:01 -0500

    Remove vi and exa test fish functions
    I stopped playing with vi locally, and went back to standard ls instead
    of exa, so this is unnecessary overhead.

--- fish/.config/fish/functions/lla.fish
+++
@@ -1,8 +0,0 @@
-function lla --description "Replace lla - long all listing - with exa if it exists"
-  if test -x /usr/local/bin/exa
-    command exa --long --all $argv
-  else
-    command ls -AFchl $argv
-  end
-end
-
--- fish/.config/fish/functions/vi.fish
+++
@@ -1,7 +0,0 @@
-function vi
-  if test -x /usr/local/bin/nvi
-    command nvi $argv
-  else
-    command vi $argv
-  end
-end
