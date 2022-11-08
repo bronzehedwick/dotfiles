0e462fbed7f5493ccbe88be958b13516e6bed8e3
Chris DeLuca <code@chrisdeluca.me>
2022-11-07 21:21:45 -0500

    Make config.fish arch independent
    Also remove a lot of stuff I probably don't need.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,5 @@
 #!/usr/bin/env fish
 
-# PATH
-
-if test -d /usr/local/sbin
-  set PATH /usr/local/sbin $PATH
-end
-
-if test -x /usr/local/bin/cargo
-  set PATH ~/.cargo/bin $PATH
-end
-
-if test -x /usr/local/bin/yarn
-  set PATH ~/.yarn/bin ~/.config/yarn/global/node_modules/.bin $PATH
-end
-
 # Neovim settings
 
 if test -z "$NVIM"
@@ -31,8 +17,9 @@ if not functions -q fisher
   curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
   fish -c fisher
 end
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
-# Bun
-set -Ux BUN_INSTALL "/Users/chris/.bun"
-set -px --path PATH "/Users/chris/.bun/bin"
+if test -x "/usr/local/opt/curl/bin"
+  set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+else if test -x "/opt/homebrew/opt/curl/bin"
+  set -g fish_user_paths "/opt/homebrew/opt/curl/bin" $fish_user_paths
+end
