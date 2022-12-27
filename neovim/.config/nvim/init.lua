9fd4df64fd7765fac166646bfa01af4e7d8b4bcd
Chris DeLuca <code@chrisdeluca.me>
2022-12-27 11:18:50 -0500

    Ensure packer is installed on first boot

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 vim.g.ts_highlight_lua = true
 
+require'plugins'
 require'configurations'
 require'mappings'
-require'plugins'
 require'colorscheme'
 
 -- vim:fdm=marker ft=lua et sts=2 sw=2 path=./lua
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+ensure_packer()
+
 -- Only required since I have packer in my `opt` pack.
 vim.cmd [[packadd packer.nvim]]
 
