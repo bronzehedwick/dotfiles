cb74c9e6a6712673b56f341aa4a4427b4ea00384
Chris DeLuca <code@chrisdeluca.me>
2023-01-09 12:50:46 -0500

    Switch to vim-sneak
    Leap is neat, but too verbose. Vim sneak is perfectly minimal, vimmy,
    and compatible.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -37,10 +37,7 @@ return require('packer').startup(function(use)
 
   -- Movement {{{
   use 'arp242/jumpy.vim'
-  use {
-    'ggandor/leap.nvim',
-    config = function() require('leap').add_default_mappings() end
-  }
+  use 'justinmk/vim-sneak'
   use 'tpope/vim-rsi'
   use 'tpope/vim-unimpaired'
   -- }}}
