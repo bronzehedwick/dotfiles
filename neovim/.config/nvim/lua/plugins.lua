29b75a0082c463a37de1c3c553d86f4e87316d76
Chris DeLuca <code@chrisdeluca.me>
2023-03-21 22:11:09 -0400

    Switch from Packer to Paq
    Much simpler, easier to maintain, and doesn't ship with features I don't
    use. Seems more maintained too, and keeps the "build on core Neovim, and
    make it a little more ergonomic, don't reinvent the wheel" philosophy.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,58 +1,32 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
-end
-
-ensure_packer()
-
--- Only required since I have packer in my `opt` pack.
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-    -- Packer can manage itself as an optional plugin {{{
-    use { 'wbthomason/packer.nvim', opt = true }
-    -- }}}
+require 'paq' {
+    'savq/paq-nvim', -- Let Paq manage itself
 
     -- Utilities {{{
-    use 'neovim/nvim-lspconfig'
-    use {
+    'neovim/nvim-lspconfig',
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-        requires = 'nvim-treesitter/nvim-treesitter',
-    }
-    use {
-        'tpope/vim-dispatch',
-        opt = true,
-        cmd = { 'Dispatch', 'Make', 'Focus', 'Start' }
-    }
-    use 'justinmk/vim-ipmotion'
-    use 'tpope/vim-repeat'
+        run = function() vim.cmd 'TSUpdate' end
+    },
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'tpope/vim-dispatch',
+    'justinmk/vim-ipmotion',
+    'tpope/vim-repeat',
     -- }}}
 
     -- Movement {{{
-    use 'arp242/jumpy.vim'
-    use 'justinmk/vim-sneak'
-    use 'tpope/vim-rsi'
-    use 'tpope/vim-unimpaired'
+    'arp242/jumpy.vim',
+    'justinmk/vim-sneak',
+    'tpope/vim-rsi',
+    'tpope/vim-unimpaired',
     -- }}}
 
     -- Git {{{
-    use 'tpope/vim-fugitive'
-    use 'tommcdo/vim-fubitive'
-    use 'tpope/vim-rhubarb'
-    use {
+    'tpope/vim-fugitive',
+    'tommcdo/vim-fubitive',
+    'tpope/vim-rhubarb',
+    {
         'lewis6991/gitsigns.nvim',
-        config = function()
+        function()
             require('gitsigns').setup {
                 on_attach = function(bufnr)
                     local gs = package.loaded.gitsigns
@@ -61,7 +35,6 @@ return require('packer').startup(function(use)
                         opts.buffer = bufnr
                         vim.keymap.set(mode, l, r, opts)
                     end
-
                     -- Navigation
                     map('n', ']c', function()
                         if vim.wo.diff then return ']c' end
@@ -89,53 +62,46 @@ return require('packer').startup(function(use)
                 end
             }
         end
-    }
+    },
     -- }}}
 
     -- Buffers {{{
-    use 'tpope/vim-eunuch'
-    use 'tpope/vim-sleuth'
-    use 'justinmk/vim-dirvish'
-    use 'mbbill/undotree'
+    'tpope/vim-eunuch',
+    'tpope/vim-sleuth',
+    'justinmk/vim-dirvish',
+    'mbbill/undotree',
     -- }}}
 
     -- Text {{{
-    use 'editorconfig/editorconfig-vim' -- TODO Remove after nvim 0.9+
-    use 'rstacruz/vim-closer'
-    use 'tpope/vim-surround'
-    use {
-        'mattn/emmet-vim',
-        opt = true
-    }
-    use {
+    'editorconfig/editorconfig-vim', -- TODO Remove after nvim 0.9+
+    'rstacruz/vim-closer',
+    'tpope/vim-surround',
+    { 'mattn/emmet-vim', opt = true },
+    {
         'nvim-orgmode/orgmode',
-        config = function()
-            require('orgmode').setup {}
-        end
-    }
-    use {
+        function() require('orgmode').setup {} end
+    },
+    {
         'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+        function() require('Comment').setup() end
+    },
     -- }}}
 
     -- Terminal {{{
-    use 'bronzehedwick/vim-primary-terminal'
+    'bronzehedwick/vim-primary-terminal',
     -- }}}
 
     -- Syntax {{{
-    use 'vim-scripts/fountain.vim'
+    'vim-scripts/fountain.vim',
     -- }}}
 
     -- Colorschemes {{{
-    use 'cormacrelf/dark-notify'
-    use {
+    'cormacrelf/dark-notify',
+    {
         'bronzehedwick/vim-colors-xcode',
         branch = 'lsp-treesitter-highlights'
-    }
+    },
     -- }}}
-end)
+}
 
 -- vim:fdm=marker ft=lua et sts=4 sw=4
