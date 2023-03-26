350d34f1cb94d6ea84939c023668adb9d2271f13
Chris DeLuca <code@chrisdeluca.me>
2023-03-26 08:17:35 -0400

    Set new path for twig files

--- neovim/.config/nvim/ftplugin/twig.lua
+++ neovim/.config/nvim/ftplugin/twig.lua
@@ -1,24 +1,15 @@
 -- Add twig pattern files to path to be able to configure below.
--- Enables `gf` on include to jump to that file. MSK specific.
-vim.opt.path:append('/Users/delucac/Sites/msk-pattern-garden/packages/twig/src/')
-
--- Set pattern for vim to recognize twig includes.
-vim.opt.include = "^/s*{%/s*include|^/s*{%/s*embed|^/s*{%/s*extends"
-
--- Remove `@` in include for file path. MSK specific.
-vim.opt.includeexpr = vim.fn.substitute('v:fname', 'mskds', '', '')
+-- Enables `gf` on include to jump to that file. Bloom specific.
+vim.opt.path = '/Users/chris/Sites/Lullabot/dsga_d8/docroot/themes/custom/ga_bloom/components'
 
 -- Use twig commenting instead of HTML.
 vim.opt.commentstring = "{# %s #}"
+
+-- Set pattern for vim to recognize twig includes.
+vim.opt.include = "^/s*{%/s*include|^/s*{%/s*embed|^/s*{%/s*extends"
 
 -- Use twiglint linter.
 vim.cmd('compiler twiglint')
 
 -- Use emmet.
 vim.cmd('packadd emmet-vim')
-
--- Add html runtime.
-vim.cmd[[
-  runtime! ftplugin/html.vim
-  runtime! ftplugin/html_*.vim ftplugin/html/*.vim
-]]
