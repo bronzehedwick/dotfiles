f3ecc57009bab334bae7ed6d88357082fb7ad7c3
Chris DeLuca <code@chrisdeluca.me>
2023-04-19 09:43:26 -0400

    Automatically insert commas in JSON files

--- neovim/.config/nvim/ftplugin/json.lua
+++ neovim/.config/nvim/ftplugin/json.lua
@@ -4,3 +4,15 @@ if vim.fn.executable('jsonlint') then
 else
   vim.cmd('compiler jsontool')
 end
+
+-- Automatically insert a comma at the end of appropriate lines.
+-- @see https://medium.com/scoro-engineering/5-smart-mini-snippets-for-making-text-editing-more-fun-in-neovim-b55ffb96325a
+vim.keymap.set('n', 'o', function()
+  local line = vim.api.nvim_get_current_line()
+  local should_add_comma = string.find(line, '[^,{[]$')
+  if should_add_comma then
+    return 'A,<cr>'
+  else
+    return 'o'
+  end
+end, { buffer = true, expr = true })
