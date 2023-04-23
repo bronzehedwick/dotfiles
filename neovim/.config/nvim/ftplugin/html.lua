f15333fda4140eaccac7b92e2ab31d153757a287
Chris DeLuca <code@chrisdeluca.me>
2023-04-23 15:06:21 -0400

    Add HTML attribute autocomplete
    Abstract the function to utilities, so it can eventually be utilized for
    other file types.

--- neovim/.config/nvim/ftplugin/html.lua
+++ neovim/.config/nvim/ftplugin/html.lua
@@ -1,2 +1,10 @@
 -- Load emmet HTML quickwrite plugin.
 vim.cmd('packadd emmet-vim')
+
+-- Auto complete quotes for HTML attributes.
+vim.keymap.set(
+  'i',
+  '=',
+  require('utilities').autocomplete_html_attribute,
+  { expr = true, buffer = true }
+)
--- neovim/.config/nvim/lua/utilities.lua
+++ neovim/.config/nvim/lua/utilities.lua
@@ -59,6 +59,26 @@ M.fuzzy_search = function(files_command, action)
     })
 end
 
+M.autocomplete_html_attribute = function()
+    -- The cursor location does not give us the correct node in this case, so
+    -- we need to get the node to the left of the cursor.
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+
+    local node = vim.treesitter.get_node { pos = left_of_cursor_range }
+    local nodes_active_in = {
+        'attribute_name',
+        'directive_argument',
+        'directive_name',
+    }
+    if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+        -- The cursor is not on an attribute node
+        return '='
+    end
+
+    return '=""<left>'
+end
+
 return M
 
 -- vim:fdm=marker ft=lua et sts=4 sw=4
