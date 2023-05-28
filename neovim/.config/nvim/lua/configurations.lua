0e648a63f18c517f1e3d632ed0c228b7c1821378
Chris DeLuca <code@chrisdeluca.me>
2023-05-28 09:55:14 -0400

    Remove LSP tag jumping mapping
    This mapping is added automatically.

--- neovim/.config/nvim/lua/configurations.lua
+++ neovim/.config/nvim/lua/configurations.lua
@@ -278,7 +278,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
-        vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
