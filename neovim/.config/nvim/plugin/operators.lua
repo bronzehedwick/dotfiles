d09d5e8708cd7302b1e50ed50d67d08a15560ed2
Chris DeLuca <code@chrisdeluca.me>
2023-06-02 08:46:46 -0400

    Add open operator
    Rename the file, too, since it serves multiple purposes.

--- neovim/.config/nvim/plugin/operators.lua
+++ neovim/.config/nvim/plugin/operators.lua
@@ -12,4 +12,18 @@ vim.cmd[[
 
   nnoremap gs <Esc>:set operatorfunc=GrepOperator<cr>g@
   vnoremap gs <Esc><c-u>call GrepOperator(visualmode())<cr>
+
+  function! OpenOperator(type)
+   if a:type ==# 'v'
+     execute 'normal! `<v`>y'
+   elseif a:type ==# 'char'
+     execute 'normal! `[v`]y'
+   else
+     return
+   endif
+   silent execute '!open ' . shellescape(@@)
+  endfunction
+
+  nnoremap go <Esc>:set operatorfunc=OpenOperator<cr>g@
+  vnoremap go <Esc><c-u>call OpenOperator(visualmode())<cr>
 ]]
