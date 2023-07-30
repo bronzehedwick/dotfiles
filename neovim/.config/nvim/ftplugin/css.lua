f97fdc0ca6c19398275abf1d83c56d31a0006e64
Chris DeLuca <code@chrisdeluca.me>
2023-07-30 12:33:43 -0400

    Correct CSS LSP
    SASS was correct, but CSS was using the old one that doesn't work
    (well).

--- neovim/.config/nvim/ftplugin/css.lua
+++ neovim/.config/nvim/ftplugin/css.lua
@@ -2,13 +2,16 @@
 vim.fn.execute('compiler stylelint')
 
 -- LSP.
-local lsp_path = '/opt/homebrew/bin/cssls'
+local lsp_path = '/opt/homebrew/bin/vscode-css-language-server'
 if vim.fn.filereadable(lsp_path) == 1 then
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
     vim.lsp.start({
         name = 'cssls',
         cmd = { 'vscode-css-language-server', '--stdio' },
-        root_dir = vim.fs.dirname(vim.fsg.find({'package.json', '.git'})[1]),
+        root_dir = vim.fs.dirname(vim.fs.find({'package.json', '.git'})[1]),
         single_file_support = true,
+        capabilities = capabilities,
     })
 end
 
