9d6e7cd8083cdfb397728ded641a3dffef85357a
Chris DeLuca <code@chrisdeluca.me>
2023-10-24 10:53:59 -0400

    Mapping to close quick fix list

--- neovim/.config/nvim/ftplugin/qf.lua
+++ neovim/.config/nvim/ftplugin/qf.lua
@@ -1,5 +1,8 @@
 -- Close quickfix list.
 vim.keymap.set('n', 'gq', ':cclose<CR>', { buffer = true, silent = true })
+-- vim.keymap.set('n', 'gq', function()
+--     local wininfo = vim.fn.getwininfo()
+-- end, { buffer = true, silent = true })
 
 -- No numbers in the quickfix list.
 vim.opt_local.number = false;
