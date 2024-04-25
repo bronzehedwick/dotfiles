e7cad89491fe9a109ba42c584cbff4f1b80e67e2
Chris DeLuca <code@chrisdeluca.me>
2024-04-25 11:02:22 -0400

    Remove habits file from orgmode agenda

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -59,7 +59,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/Documents/org/random.org" "~/Documents/org/journal.org" "~/Documents/org/projects.org" "~/Documents/org/index.org" "~/Documents/org/books.org" "~/Documents/org/bits.org" "~/Documents/org/refile.org" "~/Documents/org/habits.org")))
+   '("~/Documents/org/random.org" "~/Documents/org/journal.org" "~/Documents/org/projects.org" "~/Documents/org/index.org" "~/Documents/org/books.org" "~/Documents/org/bits.org" "~/Documents/org/refile.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
