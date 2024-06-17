7b1a497dee3195c50ada3172cf96186741dc1e56
Chris DeLuca <code@chrisdeluca.me>
2024-06-17 15:36:04 -0400

    Initialize emacs packages
    I don't know why this is needed, but I also don't know Emacs. Here it
    is.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'default-frame-alist '(font . "SF Mono-18"))
 (set-face-attribute 'default t :font "SF Mono-18")
 
