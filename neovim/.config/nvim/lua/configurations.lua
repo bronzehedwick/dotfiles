df706aa51f0242eed5d95c0e46fa8c8b7094726b
Chris DeLuca <code@chrisdeluca.me>
2024-12-17 19:40:16 -0500

    Use snipe for code action menus et al

--- neovim/.config/nvim/lua/configurations.lua
+++ neovim/.config/nvim/lua/configurations.lua
@@ -376,6 +376,13 @@ require'nvim-surround'.setup()
 local snipe = require('snipe')
 snipe.setup()
 vim.keymap.set('n', '<M-b>', snipe.open_buffer_menu)
+snipe.ui_select_menu = require("snipe.menu"):new { position = "center" }
+snipe.ui_select_menu:add_new_buffer_callback(function (m)
+  vim.keymap.set("n", "<esc>", function ()
+    m:close()
+  end, { nowait = true, buffer = m.buf })
+end)
+vim.ui.select = snipe.ui_select
 -- }}}
 
 -- Highlight colors {{{2
