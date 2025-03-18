b319d202795b13492ad42b18e92477d8c55788bd
Chris DeLuca <code@chrisdeluca.me>
2025-03-18 11:13:02 -0400

    Update today

--- fish/.config/fish/functions/today.fish
+++ fish/.config/fish/functions/today.fish
@@ -1,9 +1,8 @@
 #!/usr/bin/env fish
 
 function today
-  calendar -A 0 -f /usr/share/calendar/calendar.lotr
-  calendar -A 0 -f /usr/share/calendar/calendar.world
-  calendar -A 0 -f /usr/share/calendar/calendar.music
-  calendar -A 0 -f /usr/share/calendar/calendar.freebsd
-  calendar -A 0 -f /usr/share/calendar/calendar.computer
+  calendar -A 0 -f /usr/local/share/calendar/calendar.lotr
+  calendar -A 0 -f /usr/local/share/calendar/calendar.world
+  calendar -A 0 -f /usr/local/share/calendar/calendar.music
+  calendar -A 0 -f /usr/local/share/calendar/calendar.computer
 end
