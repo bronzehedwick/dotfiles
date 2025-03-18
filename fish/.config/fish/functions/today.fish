c281a46b241df278047931d575637dde918a6663
Chris DeLuca <code@chrisdeluca.me>
2025-03-18 11:15:11 -0400

    Remove today duplicates

--- fish/.config/fish/functions/today.fish
+++ fish/.config/fish/functions/today.fish
@@ -1,8 +1,12 @@
 #!/usr/bin/env fish
 
-function today
+function _interesting_calendars
   calendar -A 0 -f /usr/local/share/calendar/calendar.lotr
   calendar -A 0 -f /usr/local/share/calendar/calendar.world
   calendar -A 0 -f /usr/local/share/calendar/calendar.music
   calendar -A 0 -f /usr/local/share/calendar/calendar.computer
+end
+
+function today
+  _interesting_calendars|uniq
 end
