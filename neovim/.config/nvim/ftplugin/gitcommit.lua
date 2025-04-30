949d2f075b143e093fcd4aa8cd7fa6cd0ebba173
Chris DeLuca <code@chrisdeluca.me>
2025-04-30 13:00:35 -0400

    Try to get tabstops to stick in commit messages

--- neovim/.config/nvim/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/ftplugin/gitcommit.lua
@@ -1,5 +1,8 @@
 -- Turn on spell checking.
 vim.opt_local.spell = true
 
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 2
+
 -- Mapping to insert current git ticket into file.
 vim.keymap.set('n', '<LocalLeader>t', ':0read !git ticket<CR>', { silent = true })
