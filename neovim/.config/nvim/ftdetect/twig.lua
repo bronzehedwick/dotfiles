c8433f990c913b3f657b6f992d5b13235ae4555b
Chris DeLuca <637174+bronzehedwick@users.noreply.github.com>
2025-05-28 00:26:42 -0400

    Create twig.lua ftdetect

---
+++ neovim/.config/nvim/ftdetect/twig.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  pattern = '*.twig',
+  callback = function()
+    vim.api.nvim_set_option_value('filetype', 'twig', {scope='local'})
+  end
+})
