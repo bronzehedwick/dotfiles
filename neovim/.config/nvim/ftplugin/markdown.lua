04784dabd903a867779b5858ef2da56b09b381bb
Chris DeLuca <code@chrisdeluca.me>
2025-07-01 22:53:44 -0400

    Add smart list items for markdown files

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -31,4 +31,29 @@ vim.keymap.set('n', '<C-Space>', function()
     vim.cmd('normal `p')
 end, { silent = true })
 
--- vim:fdm=marker ft=lua et sts=2 sw=2
+vim.keymap.set('i', '<CR>', function()
+local line = vim.api.nvim_get_current_line()
+    local node = vim.treesitter.get_node {
+        pos = vim.api.nvim_win_get_cursor(0)
+    }
+    if not node or not node:type() == 'list_item' then
+        return '<CR>'
+    end
+    local bullet = string.match(line, '[^.]')
+    if not bullet then
+        return '<CR>'
+    end
+    local num_bullet = string.match(line, '^[%d]*%.')
+    if vim.fn.trim(line) == num_bullet then
+        return '<Esc>0Do'
+    end
+    if num_bullet then
+        return '<CR>' .. num_bullet .. ' <Esc>0<C-a>A'
+    end
+    if vim.fn.trim(line) == bullet then
+        return '<Esc>0Do'
+    end
+    return '<CR>' .. bullet .. ' '
+end, { buffer = true, expr = true })
+
+-- vim:fdm=marker ft=lua et sts=4 sw=4
