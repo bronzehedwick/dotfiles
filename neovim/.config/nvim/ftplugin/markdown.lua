2b9fd7e0d914541bb26e4461e3ce078b4d527c53
Chris DeLuca <code@chrisdeluca.me>
2025-07-03 16:45:50 -0400

    Fix markdown smart lists
    Simplify, remove treesitter, and just use regexes. This case is
    straightforward enough to just use regex, and I don't have good internet
    access right now to research treesitter more.

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -32,26 +32,17 @@ vim.keymap.set('n', '<C-Space>', function()
 end, { silent = true })
 
 vim.keymap.set('i', '<CR>', function()
-local line = vim.api.nvim_get_current_line()
-    local node = vim.treesitter.get_node {
-        pos = vim.api.nvim_win_get_cursor(0)
-    }
-    if not node or not node:type() == 'list_item' then
-        return '<CR>'
-    end
-    local bullet = string.match(line, '[^.]')
-    if not bullet then
-        return '<CR>'
-    end
+    local line = vim.fn.trim(vim.api.nvim_get_current_line())
+    local bullet = string.match(line, '^[%*|%+|%-]')
     local num_bullet = string.match(line, '^[%d]*%.')
-    if vim.fn.trim(line) == num_bullet then
+    if not bullet and not num_bullet then
+        return '<CR>'
+    end
+    if line == bullet or line == num_bullet then
         return '<Esc>0Do'
     end
     if num_bullet then
         return '<CR>' .. num_bullet .. ' <Esc>0<C-a>A'
-    end
-    if vim.fn.trim(line) == bullet then
-        return '<Esc>0Do'
     end
     return '<CR>' .. bullet .. ' '
 end, { buffer = true, expr = true })
