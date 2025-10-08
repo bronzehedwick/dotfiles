3d5b269d764aad360dd409912b7a2b295d52ce45
Chris DeLuca <code@chrisdeluca.me>
2025-10-08 15:20:25 -0400

    Disable JS object comma insertion
    It does the wrong thing sometimes. Disable it until I have the time to
    fix it.

--- neovim/.config/nvim/ftplugin/javascript.lua
+++ neovim/.config/nvim/ftplugin/javascript.lua
@@ -46,62 +46,62 @@ local is_inside_iterable = function()
     return true
 end
 
-vim.keymap.set(
-    'n',
-    'o',
-    function()
-        if not is_inside_iterable() then
-            return 'o'
-        end
-        local line = vim.api.nvim_get_current_line()
-        if string.find(line, '}') or string.find(line, ']') then
-            return 'o'
-        end
-        local needs_comma = string.find(line, '[^,{[]$')
-        if needs_comma then
-            return 'A,<CR>'
-        else
-            return 'o'
-        end
-    end,
-    { buffer = true, expr = true }
-)
+-- vim.keymap.set(
+--     'n',
+--     'o',
+--     function()
+--         if not is_inside_iterable() then
+--             return 'o'
+--         end
+--         local line = vim.api.nvim_get_current_line()
+--         if string.find(line, '}') or string.find(line, ']') then
+--             return 'o'
+--         end
+--         local needs_comma = string.find(line, '[^,{[]$')
+--         if needs_comma then
+--             return 'A,<CR>'
+--         else
+--             return 'o'
+--         end
+--     end,
+--     { buffer = true, expr = true }
+-- )
 
-vim.keymap.set(
-    'n',
-    'O',
-    function()
-        if not is_inside_iterable() then
-            return 'O'
-        end
-        local line_num = vim.fn.line('.')
-        local line_above = vim.fn.getline(line_num - 1)
-        local needs_comma = string.find(line_above, '[^,{[]$')
-        if needs_comma then
-            return '<Up>A,<CR>'
-        else
-            return 'O'
-        end
-    end,
-    { buffer = true, expr = true }
-)
+-- vim.keymap.set(
+--     'n',
+--     'O',
+--     function()
+--         if not is_inside_iterable() then
+--             return 'O'
+--         end
+--         local line_num = vim.fn.line('.')
+--         local line_above = vim.fn.getline(line_num - 1)
+--         local needs_comma = string.find(line_above, '[^,{[]$')
+--         if needs_comma then
+--             return '<Up>A,<CR>'
+--         else
+--             return 'O'
+--         end
+--     end,
+--     { buffer = true, expr = true }
+-- )
 
-vim.keymap.set(
-    'n',
-    'dd',
-    function()
-        if not is_inside_iterable() then
-            return 'dd'
-        end
-        local line = vim.api.nvim_get_current_line()
-        local needs_comma_removed = string.find(line, '[^,{[]$')
-        if needs_comma_removed then
-            return 'dd<Up>$x'
-        else
-            return 'dd'
-        end
-    end,
-    { buffer = true, expr = true }
-)
+-- vim.keymap.set(
+--     'n',
+--     'dd',
+--     function()
+--         if not is_inside_iterable() then
+--             return 'dd'
+--         end
+--         local line = vim.api.nvim_get_current_line()
+--         local needs_comma_removed = string.find(line, '[^,{[]$')
+--         if needs_comma_removed then
+--             return 'dd<Up>$x'
+--         else
+--             return 'dd'
+--         end
+--     end,
+--     { buffer = true, expr = true }
+-- )
 
 -- vim:fdm=marker ft=lua et sts=4 sw=4
