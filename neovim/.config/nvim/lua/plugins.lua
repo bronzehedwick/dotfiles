9f0f715e56a79394c8a518fefda4197763e07f94
Chris DeLuca <code@chrisdeluca.me>
2026-01-23 10:47:24 -0500

    Remove rainbow delimiters
    I don't actually think it adds much utility, and it does add extra color
    noise.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -45,7 +45,6 @@ require 'paq' {
         build = 'make install_jsregexp'
     },
     'rafamadriz/friendly-snippets',
-    'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
     -- }}}
 
     -- Syntax {{{
