cbc219785829591eabf52e8a3864ee121222a2da
Chris DeLuca <code@chrisdeluca.me>
2026-04-14 11:53:59 -0400

    Disable provider warnings

--- neovim/.config/nvim/lua/configurations.lua
+++ neovim/.config/nvim/lua/configurations.lua
@@ -1,5 +1,11 @@
 -- Interface {{{
 
+-- Disable provider warnings, since they're not in use.
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+
 -- Use soft tabs.
 vim.opt.expandtab = true
 vim.opt.tabstop = 8
